f5177af43e82a11121e9e590d9da097d9c100ad8
Nathalie Gustafsson <nath.gustafsson@gmail.com>
2023-08-17 11:19:38 +0200

    add title to products

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,5 +1,19 @@
 function Products() {
-  return <div></div>;
+  return (
+    <div style={{ marginTop: '60px' }}>
+      <h2
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          fontFamily: 'Playfair Display',
+          fontSize: '35px',
+          fontWeight: '300',
+        }}
+      >
+        New in selection of candels
+      </h2>
+    </div>
+  );
 }
 
 export default Products;
