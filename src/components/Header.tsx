14c4e6dae35929b609e090bc52be052d80685fa1
Nathalie Gustafsson <nath.gustafsson@gmail.com>
2023-08-17 14:09:07 +0200

    add a header div around the component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,56 +1,58 @@
-import '../css/desktop.css';
-import '../css/mobile.css';
-import '../css/tablet.css';
+import '../css/1 - desktop.css';
+import '../css/2 - tablet.css';
+import '../css/3 - mobile.css';
 
 function Header() {
   return (
-    <div className="headerContainer">
-      <div>
-        <div className="hamburgermenu">
+    <div className='header'>
+      <div className="headerContainer">
+        <div>
+          <div className="hamburgermenu">
+            <span
+              className="material-symbols-outlined"
+              style={{
+                fontSize: '40px',
+                fontWeight: 200,
+                marginRight: '10px',
+                cursor: 'pointer',
+              }}
+            >
+              menu
+            </span>
+          </div>
+          <ul className="ulLinks">
+            <li className="linksHeader">Home</li>
+            <li className="linksHeader">Products</li>
+          </ul>
+        </div>
+
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          <img className="logo" src="logga.png" alt="logga" />
+        </div>
+
+        <div style={{ display: 'flex' }}>
           <span
             className="material-symbols-outlined"
             style={{
-              fontSize: '40px',
+              fontSize: '45px',
               fontWeight: 200,
               marginRight: '10px',
               cursor: 'pointer',
             }}
           >
-            menu
+            person
+          </span>
+          <span
+            className="material-symbols-outlined"
+            style={{
+              fontSize: '41px',
+              fontWeight: 200,
+              cursor: 'pointer',
+            }}
+          >
+            shopping_bag
           </span>
         </div>
-        <ul className="ulLinks">
-          <li className="linksHeader">Home</li>
-          <li className="linksHeader">Products</li>
-        </ul>
-      </div>
-
-      <div style={{ display: 'flex', justifyContent: 'center' }}>
-        <img className="logo" src="logga.png" alt="logga" />
-      </div>
-
-      <div style={{ display: 'flex' }}>
-        <span
-          className="material-symbols-outlined"
-          style={{
-            fontSize: '45px',
-            fontWeight: 200,
-            marginRight: '10px',
-            cursor: 'pointer',
-          }}
-        >
-          person
-        </span>
-        <span
-          className="material-symbols-outlined"
-          style={{
-            fontSize: '41px',
-            fontWeight: 200,
-            cursor: 'pointer',
-          }}
-        >
-          shopping_bag
-        </span>
       </div>
     </div>
   );
