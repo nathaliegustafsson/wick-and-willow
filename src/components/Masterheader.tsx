288285fc6115e500f5452acdaf0b0539836ec22b
Nathalie Gustafsson <nath.gustafsson@gmail.com>
2023-08-17 14:08:47 +0200

    moved styling to css file

--- src/components/Masterheader.tsx
+++ src/components/Masterheader.tsx
@@ -1,45 +1,16 @@
-import candleimage from '../assets/candlespic.jpeg';
+import '../css/1 - desktop.css';
+import '../css/2 - tablet.css';
+import '../css/3 - mobile.css';
 
 function Masterheader() {
   return (
-    <div>
-      <h1
-        style={{
-          position: 'absolute',
-          left: '20%',
-          top: '40%',
-          zIndex: 5,
-          fontFamily: 'Playfair Display',
-          fontWeight: '200',
-          fontSize: '60px',
-        }}
-      >
-        BLOMUS
-      </h1>
-      <p
-        style={{
-          position: 'absolute',
-          left: '20%',
-          top: '56%',
-          zIndex: 5,
-          fontFamily: 'Poppins',
-          fontWeight: '400',
-          fontSize: '18px',
-        }}
-      >
-        NEW COLLABORATION WITH BLOMUS
-      </p>
-      <div
-        style={{
-          backgroundImage: `url(${candleimage})`,
-          height: '800px',
-          backgroundPosition: 'center',
-          backgroundRepeat: 'no-repeat',
-          backgroundSize: 'cover',
-          marginLeft: '120px',
-          marginRight: '120px',
-        }}
-      ></div>
+    <div className="MasterHeader">
+      <div className="MHbackground">
+        <div className="MHtextContainer">
+          <h1 className="titleMH">BLOMUS</h1>
+          <p className="underTitleMH">NEW COLLABORATION WITH BLOMUS</p>
+        </div>
+      </div>
     </div>
   );
 }
