06e7db614ab049b8b40f18832daab04182450adc
Nathalie Gustafsson <nath.gustafsson@gmail.com>
2023-08-18 15:00:30 +0200

    add content for pictureAndQuote

--- src/components/PictureAndQuote.tsx
+++ src/components/PictureAndQuote.tsx
@@ -1,5 +1,15 @@
 function PictureAndQuote() {
-  return <div></div>;
+  return (
+    <div className="pictureAndQuoteContainer">
+      <div className="picture"></div>
+      <div className="quote">
+        <h3 className="quoteText">
+          "Wick and Willow is a name that conveys a harmonious blend of
+          essential candle components and a serene, nature-inspired vibe."
+        </h3>
+      </div>
+    </div>
+  );
 }
 
 export default PictureAndQuote;
