b41a7bcacce268fca99239f2562454279513b611
Nathalie Gustafsson <nath.gustafsson@gmail.com>
2023-08-18 15:22:20 +0200

    add div around component

--- src/components/PictureAndQuote.tsx
+++ src/components/PictureAndQuote.tsx
@@ -1,12 +1,14 @@
 function PictureAndQuote() {
   return (
-    <div className="pictureAndQuoteContainer">
-      <div className="picture"></div>
-      <div className="quote">
-        <h3 className="quoteText">
-          "Wick and Willow is a name that conveys a harmonious blend of
-          essential candle components and a serene, nature-inspired vibe."
-        </h3>
+    <div className="pictureAndQuoteBigContainer">
+      <div className="pictureAndQuoteContainer">
+        <div className="picture"></div>
+        <div className="quote">
+          <h3 className="quoteText">
+            "Wick and Willow is a name that conveys a harmonious blend of
+            essential candle components and a serene, nature-inspired vibe."
+          </h3>
+        </div>
       </div>
     </div>
   );
