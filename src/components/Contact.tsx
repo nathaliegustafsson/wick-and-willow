3e9572be4dc7deb0589fad4e9d9fdbdc7d2cce32
Nathalie Gustafsson <nath.gustafsson@gmail.com>
2023-08-18 15:22:13 +0200

    add div around component

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,18 +1,20 @@
 function Contact() {
   return (
-    <div className="contactContainer">
-      <h2 className="contactTitle">Contact</h2>
-      <div>
-        <label>
-          NAME
-          <input name="name" />
-        </label>
-        <label>EMAIL</label>
-        <input name="email" />
-        <label>
-          MESSAGE
-          <textarea name="messageContent" rows={4} cols={40} />
-        </label>
+    <div className="contactBigContainer">
+      <div className="contactContainer">
+        <h2 className="contactTitle">Contact</h2>
+        <div>
+          <label>
+            NAME
+            <input name="name" />
+          </label>
+          <label>EMAIL</label>
+          <input name="email" />
+          <label>
+            MESSAGE
+            <textarea name="messageContent" rows={4} cols={40} />
+          </label>
+        </div>
       </div>
     </div>
   );
