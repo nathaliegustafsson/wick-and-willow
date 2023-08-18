f3f9a6c1e017bbbd67e2972394b395f0540a62c7
Nathalie Gustafsson <nath.gustafsson@gmail.com>
2023-08-18 13:41:41 +0200

    add about content

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,5 +1,23 @@
 function About() {
-  return <div></div>;
+  return (
+    <div className="aboutContainer">
+      <h2 className="aboutTitle">About us</h2>
+      <p className="aboutText">
+        “We're Wick & Willow, your ultimate destination for indulging in the
+        enchanting world of scented candles that seamlessly blend the warm glow
+        of candlelight with the serene elegance of nature. At Wick and Willow,
+        we believe that a candle is not just a simple source of light; it's an
+        embodiment of comfort, tranquility, and beauty, capable of transforming
+        any space into a soothing sanctuary. <br />
+        <br /> As you step into our virtual webshop, you'll be greeted by a
+        curated collection of handcrafted candles that reflect our passion for
+        capturing the essence of both the mystical wick and the graceful willow
+        tree. Each candle is a testament to the art of candle-making,
+        meticulously crafted to ensure a clean and steady burn, providing you
+        with hours of captivating illumination.”
+      </p>
+    </div>
+  );
 }
 
 export default About;
