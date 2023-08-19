217db63d8a398ae7c6954686256a92bb0b80d28a
Nathalie Gustafsson <nath.gustafsson@gmail.com>
2023-08-19 15:04:42 +0200

    add content for footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,37 @@
 function Footer() {
-  return <div></div>;
+  return (
+    <div className="footerBigContainer">
+      <div className="footerContainer">
+        <div className="logoFooterContainer">
+          <img className="logoFooter" src="./logoWide.png" alt="logoWide" />
+          <p
+            style={{
+              fontFamily: 'Poppins',
+              fontSize: '13px',
+              fontWeight: '300',
+              marginTop: '20px',
+            }}
+          >
+            Copyright 2023. All rights reserved
+          </p>
+        </div>
+        <div className="footerLinksContainer">
+          <div className="footerLinksLeft">
+            <h4 style={{ fontWeight: '600', fontSize: '13px' }}>Company</h4>
+            <p>About Us</p>
+            <p>Career</p>
+            <p>Our Products</p>
+          </div>
+          <div className="footerLinksRight">
+            <h4 style={{ fontWeight: '600', fontSize: '13px' }}>Help</h4>
+            <p>Contact</p>
+            <p>My Account</p>
+            <p>Our Stores</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
